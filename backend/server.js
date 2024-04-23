9a5c8be080f2aeced108633a09c3cb6eb6a5a305
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-23 10:23:04 -0300

    created .env  file for the backend

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,12 @@ import express from "express"
 const app = express()
 
 
+app.get('/',(req,res) => {
+    res.send("Ola Mundo")
+})
+
+const PORT = process.env.PORT || 5000
 
 app.listen(5000,() => {
-    console.log("Servidor rodando na porta 5000")
+    console.log(`Servidor rodando na porta ${PORT}`)
 })
