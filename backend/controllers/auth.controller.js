626bde87bf45d5fbff2c1defe4d9522229834a40
Diego Vilhlava <diegorocha450@yahoo.com>
2024-04-23 10:40:40 -0300

    auth controller created

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,12 @@
+
+export const signup = async(req,res) => {
+    res.send("signup ok")
+}
+
+export const login = async (req,res) => {
+    res.send("login ok")
+}
+
+export const logout = async (req,res) => {
+    res.send("logout ok")
+}
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,16 @@
 import express from "express"
-
+import dotenv from "dotenv"
+import authRoutes from "./routes/auth.routes.js"
 const app = express()
-
+dotenv.config()
+const PORT = process.env.PORT || 5000
 
 app.get('/',(req,res) => {
     res.send("Ola Mundo")
 })
 
-const PORT = process.env.PORT || 5000
+app.use('/api/auth',authRoutes)
 
-app.listen(5000,() => {
+app.listen(PORT,() => {
     console.log(`Servidor rodando na porta ${PORT}`)
 })
