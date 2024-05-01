daf9f6bd452e46510d2a341db49a137f1ff4283f
Diego Vilhlava <diegorocha450@yahoo.com>
2024-05-01 10:10:43 -0300

    preparing for deploy

--- backend/server.js
+++ backend/server.js
@@ -6,15 +6,22 @@ import authRoutes from "./routes/auth.routes.js"
 import userRoutes from "./routes/users.routes.js"
 import messageRoutes from "./routes/message.routes.js"
 import connectToMongDb from "./db/connectToMongoDb.js"
-
+import path from "path"
 dotenv.config()
 const PORT = process.env.PORT || 5000
+
+const __dirname = path.resolve()
 
 app.use(express.json())
 app.use(cookieParser())
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 app.use('/api/users', userRoutes)
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+
+app.get("*",(req,res) => {
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 server.listen(PORT, () => {
     connectToMongDb()
     console.log(`Servidor rodando na porta ${PORT}`)
